33a317ac2a674c389165b3907001ce998f7ef1c2
despliegue <desplieguedaw2@gmail.com>
2016-10-19 00:21:29 +0200

    Es tarde ya

--- Calculadora/script/script.js
+++ Calculadora/script/script.js
@@ -1,1 +1,11 @@
 'use strict';
+(function(){
+var botones = document.getElementsByTagName("button");
+for(var i=0; i<botones.length; i++){
+    botones[i].addEventListener("click", mostrar, false);
+}
+
+function mostrar(){
+    alert("estas aqui");
+}
+})();
