b8d43921f211918ae3c8faa5220ee088a05fb3ab
steevenrubiano <jsrmcol93@gmail.com>
2016-10-20 00:29:06 +0200

    liandola

--- Calculadora/script/script.js
+++ Calculadora/script/script.js
@@ -1,11 +1,40 @@
 'use strict';
-(function(){
-var botones = document.getElementsByTagName("button");
-for(var i=0; i<botones.length; i++){
-    botones[i].addEventListener("click", mostrar, false);
+
+var btnNum = document.querySelectorAll(".numeros");
+for(var i=0; i<btnNum.length; i++){
+    btnNum[i].addEventListener("click", clickNumeros, false);
 }
 
-function mostrar(){
-    alert("estas aqui");
+var btnSig = document.querySelectorAll(".signos");
+for(var i=0; i<btnSig.length; i++){
+    btnSig[i].addEventListener("click", clickSignos, false);
 }
-})();
+
+function clickNumeros(evt){
+	var clickNum = parseFloat(evt.target.innerHTML);
+   	document.getElementById("display").innerHTML += clickNum;
+	console.log(clickNum);
+}
+
+function clickSignos(evt){
+	var clickSig = evt.target.innerHTML;
+   	document.getElementById("display").innerHTML += clickSig;
+	console.log(clickSig);
+}
+
+var memoria = 0;
+function operar (valor, operador){
+	if(operador == "+"){
+		memoria = memoria + valor;
+	}
+	if(operador == "-"){
+		memoria = memoria - valor;
+	}
+	if(operador == "x"){
+		memoria = memoria * valor;
+	}
+	if(operador == "/"){
+		memoria = memoria / valor;
+	}
+	document.getElementById("display").innerHTML = memoria;
+}
