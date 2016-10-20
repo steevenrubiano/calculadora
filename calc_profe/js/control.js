720af95d8374f7981c2c149ccd2ea78e97f0ecee
manana <manana>
2016-10-20 14:02:06 +0200

    calc_profe

---
+++ calc_profe/js/control.js
@@ -0,0 +1,12 @@
+var memoria;
+calculadora = new Calculadora();
+function agregarADisplay(valor){
+    //TODO: ingresar en el input el valor que me llega
+}
+function operacion(operador){
+    //TODO: en caso de que me llegue un mas
+        //TODO: almacenar en la memoria
+    //TODO: en caso de que me llehue un igual
+        //TODO: llamar a la calculadora con el valor de memoria, el valor del display
+        calculadora.sumar(memoria, documente.getElementByID("display"));
+}
