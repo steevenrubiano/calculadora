cc7e0e2f4df646222edca042a818dfa33acef0da
manana <manana>
2016-10-21 13:45:50 +0200

    y dale con la calculadora

--- Calculadora/script/script.js
+++ Calculadora/script/script.js
@@ -1,40 +1,26 @@
 'use strict';
 
-var btnNum = document.querySelectorAll(".numeros");
-for(var i=0; i<btnNum.length; i++){
-    btnNum[i].addEventListener("click", clickNumeros, false);
+var btnNum1 = document.getElementById("boton1");
+btnNum1.addEventListener("click", mostrarNum, false);
+
+function mostrarNum(e){
+	var num1 = parseInt(e.target.value);
+	console.log(num1);
+	document.getElementById("display").innerHTML += num1;
+	return num1;
 }
 
-var btnSig = document.querySelectorAll(".signos");
-for(var i=0; i<btnSig.length; i++){
-    btnSig[i].addEventListener("click", clickSignos, false);
+var sig = document.getElementById("elMas");	
+sig.addEventListener("click", mostrarSigno, false);
+
+function mostrarSigno(e){
+	var operador = e.target.value;
+	console.log(operador);
+	document.getElementById("display").innerHTML += operador;
+	return operador;
 }
 
-function clickNumeros(evt){
-	var clickNum = parseFloat(evt.target.innerHTML);
-   	document.getElementById("display").innerHTML += clickNum;
-	console.log(clickNum);
-}
-
-function clickSignos(evt){
-	var clickSig = evt.target.innerHTML;
-   	document.getElementById("display").innerHTML += clickSig;
-	console.log(clickSig);
-}
-
-var memoria = 0;
-function operar (valor, operador){
-	if(operador == "+"){
-		memoria = memoria + valor;
-	}
-	if(operador == "-"){
-		memoria = memoria - valor;
-	}
-	if(operador == "x"){
-		memoria = memoria * valor;
-	}
-	if(operador == "/"){
-		memoria = memoria / valor;
-	}
-	document.getElementById("display").innerHTML = memoria;
+function operar(mostrarNum, mostrarSigno){
+	console.log(mostrarNum);
+	console.log(mostrarSigno);
 }
--- calc_profe/js/control.js
+++ calc_profe/js/control.js
@@ -8,5 +8,5 @@ function operacion(operador){
         //TODO: almacenar en la memoria
     //TODO: en caso de que me llehue un igual
         //TODO: llamar a la calculadora con el valor de memoria, el valor del display
-        calculadora.sumar(memoria, documente.getElementByID("display"));
+        calculadora.sumar(memoria, document.getElementByID("display"));
 }
