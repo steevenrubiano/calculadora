6431ac2970718208095d2db76b71f542ec85be16
manana <manana>
2016-10-24 10:36:37 +0200

    seguimos

--- Calculadora/script/control.js
+++ Calculadora/script/control.js
@@ -1,39 +1,40 @@
 'use strict';
+//var calculadora = new Calculadora();
 var memoria=0;
-var valor=0;
+
+//Add eventos a los numeros
 var btnNums = document.querySelectorAll(".numeros");
 for(var i=0; i<btnNums.length; i++){
 	btnNums[i].addEventListener("click", mostrarNum, false);
 }
 
+//Add eventos a los signos
+var btnSignos = document.querySelectorAll(".signos");
+for(var i=0; i<btnSignos.length; i++){
+	btnSignos[i].addEventListener("click", mostrarSigno, false);
+}
+
+
+//Funciones
 function mostrarNum(e){
 	var num = e.target.value;
 
 	document.getElementById("display").innerHTML = num;
 	console.log(num);
-	valor = parseInt(num);
-
-	/*document.getElementById("display").innerHTML += num;
-	var numCompleto = document.getElementById("display").innerHTML;
-	console.log(numCompleto);
-	valor = parseInt(numCompleto);*/
 }
 
-var btnSignos = document.querySelectorAll(".signos");
-for(var i=0; i<btnSignos.length; i++){
-	btnSignos[i].addEventListener("click", operarSigno, false);
-}
-
-function operarSigno(e){
+function mostrarSigno(e){
 	var operador = e.target.value;
+
+	document.getElementById("display").innerHTML = operador;
 	console.log(operador);
-	
-	if(operador == "+"){
+}
+
+	/*if(operador == "+"){
 		memoria = memoria + valor;
 		document.getElementById("display").innerHTML = memoria;
 	}
 
 	if(operador == "="){
 		document.getElementById("display").innerHTML = memoria + valor;
-	}
-}
+	}*/
